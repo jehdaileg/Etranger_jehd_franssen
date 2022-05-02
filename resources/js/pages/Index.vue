7afa0617104424b37f76fcdd34db0d92b1365ff6
jehdaileg <jehdailegrand12@gmail.com>
2022-05-02 19:43:13 +0200

    Install Inertia, Vue js, Tailwindcss via vite

---
+++ resources/js/pages/Index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h5 class="text-blue-800">Hello World</h5>
+    </div>
+</template>
+
+
+
---
+++ resources/js/pages/Users/IndexUser.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h5>Users Index</h5>
+    </div>
+</template>
